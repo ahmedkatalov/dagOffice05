2df35fdd8d33281df63fd566b60b589cabff664a
Akhmed <aaaakk122123@gmail.com>
2025-09-26 16:43:36 +0300

    cd

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/fowWorkProject/', 
+    base: '/dagOffice05/', 
   plugins: [react()],
 })
